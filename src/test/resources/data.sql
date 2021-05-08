b58f6280dffd5735093bbd73c89383bcc27db6d4
Luan Barbosa Ramalho <luan.b.ramalho@gmail.com>
2021-05-07 23:53:52 -0300

    Tests

---
+++ src/test/resources/data.sql
@@ -0,0 +1,20 @@
+--Breweries
+INSERT INTO brewery (name) VALUES ('Anheuser-Busch InBev');
+INSERT INTO brewery (name) VALUES ('Heineken');
+INSERT INTO brewery (name) VALUES ('Carlsberg');
+INSERT INTO brewery (name) VALUES ('Molson Coors Brewing');
+INSERT INTO brewery (name) VALUES ('Asahi');
+INSERT INTO brewery (name) VALUES ('Kirin');
+INSERT INTO brewery (name) VALUES ('Ambev');
+
+--BeerTypes
+INSERT INTO beer_type (name) VALUES ('Ale');
+INSERT INTO beer_type (name) VALUES ('Lager');
+INSERT INTO beer_type (name) VALUES ('Porter');
+INSERT INTO beer_type (name) VALUES ('Stout');
+INSERT INTO beer_type (name) VALUES ('Blonde Ale');
+INSERT INTO beer_type (name) VALUES ('Brown Ales');
+INSERT INTO beer_type (name) VALUES ('Pale Ale');
+INSERT INTO beer_type (name) VALUES ('Wheat');
+INSERT INTO beer_type (name) VALUES ('Pilsner');
+INSERT INTO beer_type (name) VALUES ('Sour Ale');
